408b7f715540225ca5fa41a644e6c8fae8a8c12d
ramon.wartala@sinnerschrader.com <ramon.wartala@sinnerschrader.com>
2019-09-29 16:15:48 +0200

    add image resizer

--- rpi_cam_obj_detect.py
+++ rpi_cam_obj_detect.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 
 IM_WIDTH = 1280
 IM_HEIGHT = 720
@@ -26,25 +27,40 @@ MODEL_NAME = 'mobilenetv2'
 
 NUM_CLASSES = 90
 CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'mobilenetv2-model.pb')
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'mobilenetv2-model-trt.pb')
+OUTPUT_NAMES = ['Logits/Softmax']
+INPUT_NAMES = ['input_1']
 
-detection_graph = tf.compat.v1.Graph()
+def get_frozen_graph(graph_file):
+    """Read Frozen Graph file from disk."""
+    with tf.io.gfile.GFile(graph_file, "rb") as f:
+        graph_def = tf.compat.v1.GraphDef()
+        graph_def.ParseFromString(f.read())
+    return graph_def
 
-with detection_graph.as_default():
-    od_graph_def = tf.compat.v1.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
+detection_graph = get_frozen_graph(PATH_TO_CKPT)
 
-    TFSess = tf.compat.v1.Session(graph=detection_graph)
+# Create session and load graph
+tf_config = tf.compat.v1.ConfigProto() 
+tf_config.gpu_options.allow_growth = True
+tf_sess = tf.Session(config=tf_config)
+tf.import_graph_def(detection_graph, name='')
 
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+for node in detection_graph.node:
+    print(node.name)
+    if 'input_' in node.name:
+        size = node.attr['shape'].shape
+        image_size = [size.dim[i].size for i in range(1, 4)]
+        break
+print("image_size: {}".format(image_size))
 
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+input_tensor_name = INPUT_NAMES[0] + ":0"
+output_tensor_name = OUTPUT_NAMES[0] + ":0"
+
+print("input_tensor_name: {}\noutput_tensor_name: {}".format(input_tensor_name, output_tensor_name))
+
+input_tensor = tf_sess.graph.get_tensor_by_name(input_tensor_name)
+output_tensor = tf_sess.graph.get_tensor_by_name(output_tensor_name)
 
 frame_rate_calc = 1
 freq = cv2.getTickFrequency()
@@ -56,27 +72,24 @@ cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 if cap.isOpened():
     window_handle = cv2.namedWindow(WIN_NAME, cv2.WINDOW_AUTOSIZE)
 
-
 frameCount = 0
 
 while cv2.getWindowProperty(WIN_NAME,0) >= 0:
 
     t1 = cv2.getTickCount()
     ret_val, frame = cap.read();
-    frame.setflags(write=1)
-    frame_expanded = np.expand_dims(frame, axis=0)
+    resized_frame = cv2.resize(frame, (224, 224))
+    resized_frame.setflags(write=1)
+    frame_expanded = np.expand_dims(resized_frame, axis=0)
+    frame_preprocessed = preprocess_input(frame_expanded)
     
-    (boxes, scores, classes, num) = TFSess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections],
-        feed_dict={image_tensor: frame_expanded})
+    preds = tf_sess.run(
+        output_tensor,
+        feed_dict={input_tensor: frame_preprocessed})
 
 
-    print(boxes)
-    print(np.squeeze(boxes))
-    print(classes)
-    print(num)
-    print(scores)
-
+    print(preds)
+    print('Predicted:', decode_predictions(preds, top=3)[0])
 
     cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
     cv2.imshow(WIN_NAME, frame)
